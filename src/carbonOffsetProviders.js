cd1e9400dff1279e5a297ab6ee0191d009ab7a11
Annalena Davis <annalenakdavis@gmail.com>
2018-07-26 10:58:07 -0400

    started four new components and updated action, app, and renewable components and started a list of carbon offset providers. No wi-fi during work session, and now nothing renders with no errors shown

---
+++ src/carbonOffsetProviders.js
@@ -0,0 +1,13 @@
+const carbonOffsetProviders = {
+        provider1 : {
+            provider : "Cool Effect",
+            website : "cooleffect.com"
+        },
+        provider2 : {
+            provider : "Terrapass",
+            website : "terrapass.com"
+        }
+
+}
+
+export default carbonOffsetProviders;
